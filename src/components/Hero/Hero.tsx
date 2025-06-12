fc9ebaf077b61825555e6ac54b0afcc7444afd63
maxjvjohansson <max.jv.johansson@gmail.com>
2025-06-12 22:15:56 +0200

    Update: Hero section with bg-color and more text/layout structure

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,18 @@
 export default function Hero() {
   return (
-    <section className="p-8">
-      <div className="max-w-5/12">
-        <h1 className="text-6xl font-bold leading-tight">
+    <section className="bg-gradient-to-r from-white via-green-50 to-emerald-50 p-8">
+      <div className="max-w-5/12 mb-16">
+        <h1 className="text-4xl md:text-6xl font-bold leading-tight">
           Din smarta <span className="text-lime-800">golf caddie</span> direkt i
           fickan
         </h1>
+      </div>
+      <div className="max-w-5/12">
+        <p className="text-xl">
+          Upplev framtidens golf med dynamiska klubbrekommendationer,
+          realtidsdata om vind, höjdskillnader och personligt anpassade
+          strategier
+        </p>
       </div>
     </section>
   );
